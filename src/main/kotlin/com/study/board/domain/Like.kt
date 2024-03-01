25f72efd8c1ebd4a374bcbe7517217f9ef2b275f
wlsdks <wlsdks12@naver.com>
2024-03-02 03:10:15 +0900

    PK 제약조건을 제거, Join 오류 수정

--- src/main/kotlin/com/study/board/domain/Like.kt
+++ src/main/kotlin/com/study/board/domain/Like.kt
@@ -1,10 +1,13 @@
 package com.study.board.domain
 
+import jakarta.persistence.ConstraintMode
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
+import jakarta.persistence.ForeignKey
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 
@@ -20,6 +23,7 @@ class Like(
     var id: Long = 0L
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(foreignKey = ForeignKey(ConstraintMode.NO_CONSTRAINT)) // fk 제약조건을 제거
     var post: Post = post
         protected set
 }
--- src/main/kotlin/com/study/board/domain/Tag.kt
+++ src/main/kotlin/com/study/board/domain/Tag.kt
@@ -1,10 +1,13 @@
 package com.study.board.domain
 
+import jakarta.persistence.ConstraintMode
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
+import jakarta.persistence.ForeignKey
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 
 @Entity
@@ -22,6 +25,7 @@ class Tag(
         protected set
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(foreignKey = ForeignKey(ConstraintMode.NO_CONSTRAINT)) // fk 제약조건을 제거
     var post: Post = post
         protected set
 }
